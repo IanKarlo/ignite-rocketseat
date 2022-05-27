0bf39fdd3a7dd6fb40301d04652c52a02a343081
IanKarlo <iankarlots@gmail.com>
2022-05-27 00:20:35 -0300

    add

--- nodejs/API/src/modules/cars/useCases/importCategories/ImportCategoryUseCase.ts
+++ nodejs/API/src/modules/cars/useCases/importCategories/ImportCategoryUseCase.ts
@@ -29,6 +29,7 @@ class ImportCategoryUseCase {
           });
         })
         .on("end", () => {
+          fs.promises.unlink(file.path);
           resolve(importedCategories);
         })
         .on("error", (error) => {
